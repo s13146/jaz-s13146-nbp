b99d43d7c8e4f2d6cb1a99b213436bfb469f69e7
s13146-pj <56793702+s13146@users.noreply.github.com>
2022-06-18 09:28:15 +0200

    Initial commit

---
+++ src/test/java/jazs13146nbp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jazs13146nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
